5c489f98a209f005164e83920e683c4c4196f81a
chesspaul <97707648+chesspaul@users.noreply.github.com>
2022-01-13 16:48:58 -0600

    Add files via upload

---
+++ SATemplate-main/miprimerprograma/sketch.js
@@ -0,0 +1,9 @@
+console.log("hola");
+console.log("Estoy bien");
+console.log("Como te va?");
+console.log("Pues he estado con muchas actividades, y tu?");
+console.log("Todo bien, con salud y pensando en que cenar");
+console.log("Que bueno escuchar eso, te va bien y eso me hace feliz");
+console.log("Muchas gracias amigo, hay que platicar mas seguido");
+console.log("que asi sea, nos vemos");
+console.log("hasta luego");
